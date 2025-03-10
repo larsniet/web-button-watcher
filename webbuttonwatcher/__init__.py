5d9258ba613d0797337b671252eb25661359595d
Lars van der Niet <lvdnbusiness@icloud.com>
2025-03-10 19:51:42 +0100

    Bump version to 0.1.18

--- webbuttonwatcher/__init__.py
+++ webbuttonwatcher/__init__.py
@@ -5,7 +5,7 @@ import logging
 import argparse
 from importlib.metadata import version, PackageNotFoundError
 
-__version__ = "0.1.17"
+__version__ = "0.1.18"
 
 # Set up logging
 logging.basicConfig(
